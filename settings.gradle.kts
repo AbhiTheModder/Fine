69cbde4e39851ea45027d8778f7d6b8e98e39d04
Abhi <allinoneallinone00@gmail.com>
2024-10-29 04:59:28 +0530

    make androidide compatible

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,19 @@
 pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        maven { url=uri("https://jitpack.io") }
-
-        mavenCentral()
-        gradlePluginPortal()
-    }
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
 rootProject.name = "PineExample"
+
 include(":app")
