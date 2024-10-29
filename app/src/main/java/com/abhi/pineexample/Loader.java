ad98a66c65af0a98ec1c762ede667cff7a156152
Abhi <allinoneallinone00@gmail.com>
2024-10-30 03:48:36 +0530

    v3

--- app/src/main/java/com/abhi/pineexample/Loader.java
+++ app/src/main/java/com/abhi/pineexample/Loader.java
@@ -1,13 +1,29 @@
 package com.abhi.pineexample;
 
-import android.app.AppComponentFactory;
+import android.app.Activity;
+import android.content.Intent;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.AppComponentFactory;
 
 public class Loader extends AppComponentFactory {
-    static {
-        try {
-            new context();
-        } catch (Throwable ignored) {
+
+    private static boolean initialized = false;
+
+    private void initialize() {
+        if (!initialized) {
+            try {
+                new context();
+                initialized = true;
+            } catch (Throwable ignored) {
+            }
         }
+    }
+
+    @NonNull
+    @Override
+    public Activity instantiateActivityCompat(@NonNull ClassLoader cl, @NonNull String className, Intent intent) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        initialize();
+        return super.instantiateActivityCompat(cl, className, intent);
     }
 }
